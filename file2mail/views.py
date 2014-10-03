ee71abe9b678aef564605b9d7cc3ca8790467904
Sencer H <recnes@recnes.com>
2014-10-03 11:11:31 +0300

    Nothing accomplished yet

--- file2mail/views.py
+++ file2mail/views.py
@@ -2,11 +2,12 @@ from pyramid import config
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/index.pt')
+@view_config(route_name='home', renderer='templates/index.html')
 def index(request):
     return {'project': 'File2Mail'}
 
-@view_config(route_name='docs', renderer='templates/docs.pt')
+
+@view_config(route_name='docs', renderer='templates/docs.html')
 def docs(request):
 
     return {'project': 'Documents', 'documentation': 1}
