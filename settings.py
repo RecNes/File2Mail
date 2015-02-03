4a941a2431842046b55dc0463a086d326cb43a0d
Sencer H <sencer.hamarat(at`gmail.com>
2015-02-03 17:08:40 +0200

    PROJECT_ROOT eklendi.

--- settings.py
+++ settings.py
@@ -1,3 +1,9 @@
-__author__ = 'YFY'
+# -*- coding: utf-8 -*-
+import os
+
+__author__ = 'Sencer Hamarat'
 
 
+class Settings():
+    DEBUG = False
+    PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
