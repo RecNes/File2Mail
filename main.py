a8cf42fbf314238b7837e5582b5bdfc87d335e23
Sencer H <sencer.hamarat(at`gmail.com>
2015-02-05 16:12:32 +0200

    Main Class ready for tests

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
 # -*- coding: utf-8 -*-
-from file_ops import MoveSentFiles
+from file_ops import GetFileList, MoveSentFile
+from logger import log
 from send_by_email import Email
+from settings import SETTINGS
+
 
 __author__ = 'Sencer Hamarat'
 
 
+class Main():
+    def __init__(self):
+        self.email = Email()
+
+    def start_and_run(self):
+        file_list = GetFileList().full_file_list
+
+        if len(file_list):
+            sent_files = self.email.send(host=SETTINGS["host"], port=SETTINGS["port"], tls=SETTINGS["tls"],
+                                         user=SETTINGS["user"], password=SETTINGS["password"],
+                                         sender=SETTINGS["sender"], recipients=SETTINGS["recipients"],
+                                         attachments=file_list)
+        else:
+            raise Exception("There is no file found.")
+
+        if len(sent_files):
+            move_files = MoveSentFile(target_dir=None, files=sent_files)
+            move_files.do()
+        else:
+            raise Exception("No files were moved")
+
+
 def main():
-    email = Email()
-    sent_files = email.send()
-    if len(sent_files):
-        MoveSentFiles(sent_files)
-    exit()
+    try:
+        Main().start_and_run()
+    except Exception as e:
+        log.exception(e.message)
+    finally:
+        exit()
 
 if __name__ == "__main__":
     main()
