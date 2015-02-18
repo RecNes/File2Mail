b1a6d9a92c23ef418a9e15ab4509dcc5dd22b228
Sencer H <sencer.hamarat(at`gmail.com>
2015-02-18 17:55:32 +0200

    started to append cryptography for hide password

--- settings.py
+++ settings.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import ast
+import base64
 import os
+import uuid
+from Crypto.Cipher import AES
 
 __author__ = 'Sencer Hamarat'
 
@@ -14,26 +17,84 @@ class ConfigLoader():
         self.config = dict()
         self.file_name = "file2mail.conf"
         self.file = "{root}/{file}".format(root=PROJECT_ROOT, file=self.file_name)
+        # Prepare crypto method
+        self.padding = '{'
+        self.block_size = 32
+        self.pad = lambda s: s + (self.block_size - len(s) % self.block_size) * self.padding
+        self.EncodeAES = lambda c, s: base64.b64encode(c.encrypt(self.pad(s)))
+        self.DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(self.padding)
 
-    def read_config(self):
+    def __genkey(self):
         """
-        Reads settings from file2mail.conf file and applies evaluations to necessary values
-        :return: dict()
+        Generates encryption key for hiding password written in config file.
+        :return:
         """
-        if os.path.isfile(self.file):
-            with open(self.file, mode='r') as _file:
-                self.content = _file.readlines()
-        else:
-            raise Exception("Configuration file not found: {file}".format(file=self.file))
+        if "secret" not in self.config:
+            try:
+                config_file = open("log.txt", "a")
+                try:
+                    self.config["secret"] = str(uuid.uuid4().get_hex().upper()[0:16])
+                    config_file.write("secret = {secret}".format(secret=self.config["secret"]))
+                finally:
+                    config_file.close()
+            except IOError:
+                pass
 
+    def __encode_pass(self):
+        """
+        Encodes given string with secret
+        :return:
+        """
+        self.__genkey()
+        cipher = AES.new(self.config["secret"])
+        return self.EncodeAES(cipher, self.config["password"])
+
+    def _encrypt_password(self):
+        """
+        Does encryption on password
+        :return:
+        """
         try:
-            for line in self.content:
-                if (not line.startswith('#')) and ('=' in line):
-                    line = line.replace('\r', '').replace('\n', '')
-                    key, value = line.split('=')
-                    self.config[key.strip()] = value.strip()
+            return self.__encode_pass()
+        except:
+            return False
+            # raise Exception("Unable to encode given string")
+
+    def __decode_pass(self):
+        """
+        Decodes given string with secret
+        :return:
+        """
+        self.__genkey()
+        cipher = AES.new(self.config["secret"])
+        return self.DecodeAES(cipher, self.config["password"])
+
+    def _decrypt_password(self):
+        """
+        Does decryption on password
+        :return:
+        """
+        try:
+            return self.__decode_pass()
+        except:
+            return False
+            # raise Exception("Unable to decode given string")
+
+    def _check_password_encryption(self):
+        try:
+            self._decrypt_password()
         except Exception as e:
-            raise Exception(e.message.join("Error in configuration file: {file}".format(file=self.file)))
+            print e
+            self._encrypt_password()
+        finally:
+            self._decrypt_password()
+
+    def _control_config(self):
+        """
+        Control the entries in configuration files.
+        :return:
+        """
+        self._check_password_encryption()
 
         if "target_directory" not in self.config or not self.config["target_directory"]:
             raise Exception("Target directory is not specified")
@@ -62,6 +123,28 @@ class ConfigLoader():
             self.config["recipients"] = ast.literal_eval(self.config["recipients"])
         except SyntaxError as e:
             self.config["recipients"] = []
+
+    def read_config(self):
+        """
+        Reads settings from file2mail.conf file and applies evaluations to necessary values
+        :return: dict()
+        """
+        if os.path.isfile(self.file):
+            with open(self.file, mode='r') as _file:
+                self.content = _file.readlines()
+        else:
+            raise Exception("Configuration file not found: {file}".format(file=self.file))
+
+        try:
+            for line in self.content:
+                if (not line.startswith('#')) and ('=' in line):
+                    line = line.replace('\r', '').replace('\n', '')
+                    key, value = line.split('=')
+                    self.config[key.strip()] = value.strip()
+        except Exception as e:
+            raise Exception(e.message.join("Error in configuration file: {file}".format(file=self.file)))
+
+        self._control_config()
 
         return self.config
 
